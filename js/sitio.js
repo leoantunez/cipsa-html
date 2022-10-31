3cdf11b587a1d516922c75dbb99502a89bd3d261
Leonardo D. Antunez <leonardo.d.antunez@gmail.com>
2022-10-31 21:46:58 +0100

    Practica JS

---
+++ js/sitio.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $( "#tabs" ).tabs();
+    $('.carousel').slick({
+        arrows: false,
+        dots: true,
+    });
+    $(".enlace_menu").mouseover(function(evt){
+        $(this).animate({ deg: -10 },
+            {
+              duration: 100,
+              step: function(now) {
+                $(this).css({
+                     transform: 'rotate(' + now + 'deg)' ,
+                     boxShadow: '1px 1px 1px #000'
+                });
+              }
+        });
+    });
+
+    $(".enlace_menu").mouseleave(function(evt){
+        $(this).animate({ deg: 0 },
+            {
+              duration: 100,
+              step: function(now) {
+                $(this).css({ 
+                    transform: 'rotate(' + now + 'deg)' ,
+                    boxShadow: 'none'
+                });
+              }
+        });
+    });
+
+    $( "#accordion" ).accordion();
+    $( ".datepicker" ).datepicker({
+      dateFormat: "dd-mm-yy",
+      onSelect: function(fecha){
+        $("#fecha").val(fecha);
+      }
+    });
+});
